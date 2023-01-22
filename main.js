89c2282136fa20739d5202692f778d7641da93fc
Yehor Novikov <egor.novikov@gmail.com>
2023-01-22 15:29:33 +0200

    проект секундомер

---
+++ main.js
@@ -0,0 +1,28 @@
+let counter = 0;
+const counterEl = document.querySelector('#counter');
+let timerId;
+
+
+const btnStart = document.querySelector('#start');
+const btnStop = document.querySelector('#stop');
+const btnreset = document.querySelector('#reset');
+
+
+btnStart.addEventListener('click', function () {
+    timerId = setInterval(function () {
+        counter++;
+        counterEl.innerText = counter
+    }, 1000);
+    console.log('timerId', timerId)
+});
+
+btnStop.addEventListener('click', function () {
+    clearInterval(timerId)
+})
+
+btnreset.addEventListener('click', function () {
+    console.log('reset')
+    counter = 0;
+    counterEl.innerText = counter
+
+})
